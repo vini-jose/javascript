01f4689b3bbd2d56ebcb5eba103f7f0f4eb451f2
vini-jose <assisjose481@gmail.com>
2024-02-15 23:52:43 -0300

    Estudei estrutura de repetição com teste lógico no inicio e estrutura de repetição com teste lógico no final

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,25 @@
+// Estrutra de repetição com teste lógico no final
+var c = 1
+do {
+    console.logo(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/* 
+Estrutura de repetição com teste lógico no início
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ 
+}
+
+
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+console.log("Tudo bem?")
+*/
+
